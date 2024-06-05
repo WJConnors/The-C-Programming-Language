ce3c068a596597d19d754f69a925b649d912de69
WJConnors <billy.connors@yahoo.co.uk>
2024-06-05 16:14:31 +0100

    Non-functional obstacles and structs

--- GDTV_Raylib/Dapper-Dasher/dasher.cpp
+++ GDTV_Raylib/Dapper-Dasher/dasher.cpp
@@ -1,24 +1,52 @@
 #include "raylib.h"
 
+struct AnimData
+{
+    Rectangle rec;
+    Vector2 pos;
+    int frame;
+    float updateTime;
+    float runningTime;
+};
+
 int main()
 {
-    const int windowWidth{512};
-    const int windowHeight{380};
-    InitWindow(windowWidth, windowHeight, "Dapper Dasher");
+    const int windowDimensions[2]{512, 380};
+    InitWindow(windowDimensions[0], windowDimensions[1], "Dapper Dasher");
 
     const int gravity{1000};
 
-    Texture2D scarfy = LoadTexture("textures/scarfy.png");
-    Rectangle scarfyRec = {.x = 0, .y = 0, .width = (float)scarfy.width / 6, .height = (float)scarfy.height};
-    Vector2 scarfyPos = {.x = windowWidth/2 - scarfyRec.width/2, .y = windowHeight - scarfyRec.height};
 
-    int frame{0};
-    const float updateTime = 1.0/12.0;
-    float runningTime{0};
+    Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
+    AnimData nebData{
+        {0.0f, 0.0f, (float)nebula.width/8, (float)nebula.height/8},
+        {(float)windowDimensions[0], (float)windowDimensions[1] - nebula.height/8},
+        0,
+        1.0f/12.0f,
+        0.0f
+    };
+    AnimData nebData2{
+        {0.0f, 0.0f, (float)nebula.width/8, (float)nebula.height/8},
+        {(float)windowDimensions[0] + 300, (float)windowDimensions[1] - nebula.height/8},
+        0,
+        1.0f/16.0f,
+        0.0f
+    };
+
+    int nebVel{-200};
+
+    Texture2D scarfy = LoadTexture("textures/scarfy.png");
+    AnimData scarfyData{
+        {0.0f, 0.0f, (float)scarfy.width/6, (float)scarfy.height},
+        {(float)windowDimensions[0]/2 - (scarfy.width/6)/2, (float)windowDimensions[1] - scarfy.height},
+        0,
+        1.0f / 12.0f,
+        0.0f
+    };
 
     bool isInAir{false};
     const int jumpVelocity{-600};
-    int velocity{0};    
+    int scarfyVel{0};    
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -28,40 +56,71 @@ int main()
         BeginDrawing();
         ClearBackground(WHITE);        
 
-        if(scarfyPos.y > windowHeight - scarfyRec.height)
+        if(scarfyData.pos.y > windowDimensions[1] - scarfyData.rec.height)
         {
-            velocity = 0;
+            scarfyVel = 0;
             isInAir = false;
         }
         else
         {
-            velocity += gravity * dT;
+            scarfyVel += gravity * dT;
         }
         if(IsKeyDown(KEY_SPACE) && !isInAir)
         {
-            velocity += jumpVelocity;
+            scarfyVel += jumpVelocity;
             isInAir = true;
         }
-        scarfyPos.y += velocity * dT;
 
-        runningTime += dT;
+        nebData.pos.x += nebVel * dT;
+        scarfyData.pos.y += scarfyVel * dT;
+        nebData2.pos.x += nebVel * dT;
+
+        if (!isInAir)
+        {
+            scarfyData.runningTime += dT;
+            if (scarfyData.runningTime >= scarfyData.updateTime)
+            {            
+                scarfyData.runningTime = 0.0f;
+                scarfyData.frame++;
+                if(scarfyData.frame > 5)
+                {
+                    scarfyData.frame = 0;
+                } 
+                scarfyData.rec.x = scarfyData.frame * scarfyData.rec.width;              
+            }
+        }        
         
-        if (runningTime >= updateTime)
-        {            
-            runningTime = 0;
-            scarfyRec.x = frame * scarfyRec.width;
-            frame++;
-            if(frame > 5)
+        nebData.runningTime += dT;
+        if (nebData.runningTime >= nebData.updateTime)
+        {
+            nebData.runningTime = 0.0f;
+            nebData.rec.x = nebData.frame * nebData.rec.width;
+            nebData.frame++;
+            if(nebData.frame > 7)
             {
-            frame = 0;
-            }               
+                nebData.frame = 0;
+            }            
         }
-             
 
-        DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
+        nebData2.runningTime += dT;
+        if (nebData2.runningTime >= nebData2.updateTime)
+        {
+            nebData2.runningTime = 0.0f;
+            nebData2.rec.x = nebData2.frame * nebData2.rec.width;
+            nebData2.frame++;
+            if(nebData2.frame > 7)
+            {
+                nebData2.frame = 0;
+            }            
+        }
+
+        DrawTextureRec(nebula, nebData.rec, nebData.pos, WHITE);            
+        DrawTextureRec(scarfy, scarfyData.rec, scarfyData.pos, WHITE);
+        DrawTextureRec(nebula, nebData2.rec, nebData2.pos, RED);
 
         EndDrawing();
     }
     UnloadTexture(scarfy);
+    UnloadTexture(nebula);
     CloseWindow();
 }
